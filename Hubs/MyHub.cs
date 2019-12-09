90f9cfaee1f11e67b3422e0ea884483500514ee6
Ala-Uddin <alauddin403695@gmail.com>
2019-12-09 18:42:01 +0600

    Add project files.

---
+++ Hubs/MyHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using SignalRWebChat.Models;
+
+namespace SignalRWebChat.Hubs
+{
+    public class MyHub : Hub
+    {
+        private ChatContext db = new ChatContext();
+        public void GetAllMessage()
+        {
+            List<Chat> chat = db.Chats.ToList();
+            Clients.Caller.GetAll(chat);
+        }
+        public void SendMessage(string user, string message)
+        {
+            Chat chat = new Chat()
+            {
+                Sender = user,
+                Content = message
+            };
+
+            db.Chats.Add(chat);
+            db.SaveChanges();
+
+            Clients.All.Get(chat);
+        }
+    }
+}
---
+++ Models/Chat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SignalRWebChat.Models
+{
+    public class Chat
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string Sender { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime Time { get; set; } = DateTime.Now;
+    }
+    public class ChatContext : DbContext
+    {
+        public DbSet<Chat> Chats { get; set; }
+        public ChatContext() : base("ChatConnection")
+        {
+
+        }
+    }
+}
